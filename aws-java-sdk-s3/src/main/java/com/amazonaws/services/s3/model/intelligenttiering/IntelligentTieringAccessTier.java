df4364d5458a0b56784c4d63b6adf38159724804
AWS <>
2020-11-09 19:42:41 +0000

    AWS SDK for Java 1.11.897

--- aws-java-sdk-s3/src/main/java/com/amazonaws/services/s3/model/intelligenttiering/IntelligentTieringAccessTier.java
+++ aws-java-sdk-s3/src/main/java/com/amazonaws/services/s3/model/intelligenttiering/IntelligentTieringAccessTier.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -13,22 +13,34 @@
  * permissions and limitations under the License.
  */
 
-package com.amazonaws.services.s3.model;
+package com.amazonaws.services.s3.model.intelligenttiering;
 
-public enum CompressionType {
-    NONE("NONE"),
-    GZIP("GZIP"),
-    BZIP2("BZIP2");
+/**
+ * Represents the available archive tiers for objects in the S3 Intelligent-Tiering storage class.
+ *
+ * @see com.amazonaws.services.s3.model.StorageClass
+ */
+public enum IntelligentTieringAccessTier {
 
-    private final String compressionType;
+    /**
+     * First level of tiering.
+     */
+    ARCHIVE_ACCESS("ARCHIVE_ACCESS"),
 
-    CompressionType(String compressionType) {
-        this.compressionType = compressionType;
+    /**
+     * Second level of tiering.
+     */
+    DEEP_ARCHIVE_ACCESS("DEEP_ARCHIVE_ACCESS");
+
+    private final String intelligentTieringAccessTier;
+
+    IntelligentTieringAccessTier(String intelligentTieringAccessTier) {
+        this.intelligentTieringAccessTier = intelligentTieringAccessTier;
     }
 
     @Override
     public String toString() {
-        return compressionType;
+        return intelligentTieringAccessTier;
     }
 
     /**
@@ -37,12 +49,12 @@ public enum CompressionType {
      * @param value real value
      * @return enum corresponding to the value
      */
-    public static CompressionType fromValue(String value) {
+    public static IntelligentTieringAccessTier fromValue(String value) {
         if (value == null || "".equals(value)) {
             throw new IllegalArgumentException("Value cannot be null or empty!");
         }
 
-        for (CompressionType enumEntry : CompressionType.values()) {
+        for (IntelligentTieringAccessTier enumEntry : IntelligentTieringAccessTier.values()) {
             if (enumEntry.toString().equals(value)) {
                 return enumEntry;
             }
--- aws-java-sdk-s3/src/main/java/com/amazonaws/services/s3/model/intelligenttiering/IntelligentTieringStatus.java
+++ aws-java-sdk-s3/src/main/java/com/amazonaws/services/s3/model/intelligenttiering/IntelligentTieringStatus.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -13,22 +13,25 @@
  * permissions and limitations under the License.
  */
 
-package com.amazonaws.services.s3.model;
+package com.amazonaws.services.s3.model.intelligenttiering;
 
-public enum CompressionType {
-    NONE("NONE"),
-    GZIP("GZIP"),
-    BZIP2("BZIP2");
+/**
+ * Represents whether an S3 Intelligent-Tiering configuration is enabled or disabled.
+ */
+public enum IntelligentTieringStatus {
 
-    private final String compressionType;
+    Enabled("Enabled"),
+    Disabled("Disabled");
 
-    CompressionType(String compressionType) {
-        this.compressionType = compressionType;
+    private final String intelligentTieringStatus;
+
+    IntelligentTieringStatus(String intelligentTieringStatus) {
+        this.intelligentTieringStatus = intelligentTieringStatus;
     }
 
     @Override
     public String toString() {
-        return compressionType;
+        return intelligentTieringStatus;
     }
 
     /**
@@ -37,12 +40,12 @@ public enum CompressionType {
      * @param value real value
      * @return enum corresponding to the value
      */
-    public static CompressionType fromValue(String value) {
+    public static IntelligentTieringStatus fromValue(String value) {
         if (value == null || "".equals(value)) {
             throw new IllegalArgumentException("Value cannot be null or empty!");
         }
 
-        for (CompressionType enumEntry : CompressionType.values()) {
+        for (IntelligentTieringStatus enumEntry : IntelligentTieringStatus.values()) {
             if (enumEntry.toString().equals(value)) {
                 return enumEntry;
             }
