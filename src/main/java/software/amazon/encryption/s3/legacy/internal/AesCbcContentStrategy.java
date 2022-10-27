9970104cba3af9256d81157359ff22f1cb5b00eb
Justin Plasmeier <76071473+justplaz@users.noreply.github.com>
2022-10-26 17:10:25 -0700

    feat: Adds CBC stream decryption (#25)
    * feat: Adds CBC stream decryption

--- src/main/java/software/amazon/encryption/s3/legacy/internal/AesCbcContentStrategy.java
+++ src/main/java/software/amazon/encryption/s3/legacy/internal/AesCbcContentStrategy.java
@@ -1,28 +1,21 @@
 package software.amazon.encryption.s3.legacy.internal;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import javax.crypto.BadPaddingException;
+import java.security.GeneralSecurityException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-import software.amazon.awssdk.utils.IoUtils;
 import software.amazon.encryption.s3.S3EncryptionClientException;
 import software.amazon.encryption.s3.algorithms.AlgorithmSuite;
+import software.amazon.encryption.s3.internal.CbcCipherInputStream;
 import software.amazon.encryption.s3.internal.ContentDecryptionStrategy;
 import software.amazon.encryption.s3.internal.ContentMetadata;
 import software.amazon.encryption.s3.materials.DecryptionMaterials;
 
 /**
- * This class will decrypt (only) data according for AES/CBC
+ * This class will decrypt (only) data using AES/CBC
  */
 public class AesCbcContentStrategy implements ContentDecryptionStrategy {
 
@@ -33,33 +26,19 @@ public class AesCbcContentStrategy implements ContentDecryptionStrategy {
     @Override
     public InputStream decryptContent(ContentMetadata contentMetadata, DecryptionMaterials materials,
                                       InputStream ciphertextStream) {
-        // TODO: AES-CBC should always use a stream cipher.
-        byte[] ciphertext;
-        try {
-            ciphertext = IoUtils.toByteArray(ciphertextStream);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
         AlgorithmSuite algorithmSuite = contentMetadata.algorithmSuite();
         SecretKey contentKey = new SecretKeySpec(materials.plaintextDataKey(), algorithmSuite.dataKeyAlgorithm());
         byte[] iv = contentMetadata.contentNonce();
-        final Cipher cipher;
-        byte[] plaintext;
         try {
-            cipher = Cipher.getInstance(algorithmSuite.cipherName());
+            // TODO: Allow configurable Cryptographic provider
+            final Cipher cipher = Cipher.getInstance(materials.algorithmSuite().cipherName());
             cipher.init(Cipher.DECRYPT_MODE, contentKey, new IvParameterSpec(iv));
-            plaintext = cipher.doFinal(ciphertext);
-        } catch (NoSuchAlgorithmException
-                 | NoSuchPaddingException
-                 | InvalidAlgorithmParameterException
-                 | InvalidKeyException
-                 | IllegalBlockSizeException
-                 | BadPaddingException e) {
-            throw new S3EncryptionClientException("Unable to " + algorithmSuite.cipherName() + " content decrypt.", e);
+            return new CbcCipherInputStream(ciphertextStream, cipher);
+        } catch (GeneralSecurityException ex) {
+            throw new S3EncryptionClientException("Unable to build cipher: " + ex.getMessage()
+                    + "\nMake sure you have the JCE unlimited strength policy files installed and "
+                    + "configured for your JVM.", ex);
         }
-
-        return new ByteArrayInputStream(plaintext);
     }
 
     public static class Builder {
