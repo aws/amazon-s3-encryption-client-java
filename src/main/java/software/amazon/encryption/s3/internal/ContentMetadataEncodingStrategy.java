967f973eb6d0b69f25a5d9f260195b2e5b869c18
J Plasmeier <justplaz@amazon.com>
2022-11-30 10:21:10 -0800

    fix some Checkstyle errors

--- src/main/java/software/amazon/encryption/s3/internal/ContentMetadataEncodingStrategy.java
+++ src/main/java/software/amazon/encryption/s3/internal/ContentMetadataEncodingStrategy.java
@@ -2,7 +2,6 @@ package software.amazon.encryption.s3.internal;
 
 import software.amazon.awssdk.services.s3.model.CreateMultipartUploadRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.S3Request;
 import software.amazon.encryption.s3.materials.EncryptionMaterials;
 
 public interface ContentMetadataEncodingStrategy {
