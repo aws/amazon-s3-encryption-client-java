9a96e7172ed7e8c6175c382f0fe22cc66692b7e5
Justin Plasmeier <76071473+justplaz@users.noreply.github.com>
2023-01-30 14:13:19 -0800

    Test object key (#71)
    * appends a unique string to each object key to avoid collisions when multiple tests run at the same time
    
    ---------
    
    Co-authored-by: Darwin Chowdary <imabhichow@gmail.com>

--- src/main/java/software/amazon/encryption/s3/S3EncryptionClient.java
+++ src/main/java/software/amazon/encryption/s3/S3EncryptionClient.java
@@ -132,7 +132,6 @@ public class S3EncryptionClient implements S3Client {
                 .cryptoMaterialsManager(_cryptoMaterialsManager)
                 .secureRandom(_secureRandom)
                 .build();
-
         return pipeline.putObject(putObjectRequest, requestBody);
     }
 
