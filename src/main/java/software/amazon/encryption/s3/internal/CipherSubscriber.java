b4700bddd076bb76d614faa2c6ea116b8a5eb461
J Plasmeier <justplaz@amazon.com>
2023-03-08 17:46:27 -0800

    forward IllegalStateException to avoid hanging on stream resets

--- src/main/java/software/amazon/encryption/s3/internal/CipherSubscriber.java
+++ src/main/java/software/amazon/encryption/s3/internal/CipherSubscriber.java
@@ -35,7 +35,13 @@ public class CipherSubscriber implements Subscriber<ByteBuffer> {
 
         if (amountToReadFromByteBuffer > 0) {
             byte[] buf = BinaryUtils.copyBytesFrom(byteBuffer, amountToReadFromByteBuffer);
-            outputBuffer = cipher.update(buf, 0, amountToReadFromByteBuffer);
+            try {
+                outputBuffer = cipher.update(buf, 0, amountToReadFromByteBuffer);
+            } catch (final IllegalStateException exception) {
+                // TODO: Implement retries. For now, forward and rethrow.
+                this.onError(exception);
+                throw exception;
+            }
             if (outputBuffer == null && amountToReadFromByteBuffer < cipher.getBlockSize()) {
                 // The underlying data is too short to fill in the block cipher
                 // This is true at the end of the file, so complete to get the final
