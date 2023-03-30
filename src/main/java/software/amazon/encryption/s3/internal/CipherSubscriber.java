52a351d458db01b4787cb6939433ff3ed565d97e
J Plasmeier <justplaz@amazon.com>
2023-03-29 17:08:59 -0700

    fix: remove redundant retry/reinit logic

--- src/main/java/software/amazon/encryption/s3/internal/CipherSubscriber.java
+++ src/main/java/software/amazon/encryption/s3/internal/CipherSubscriber.java
@@ -47,16 +47,7 @@ public class CipherSubscriber implements Subscriber<ByteBuffer> {
 
         if (amountToReadFromByteBuffer > 0) {
             byte[] buf = BinaryUtils.copyBytesFrom(byteBuffer, amountToReadFromByteBuffer);
-            try {
-                outputBuffer = cipher.update(buf, 0, amountToReadFromByteBuffer);
-            } catch (final IllegalStateException exception) {
-                // This happens when the stream is reset and the cipher is reused with the
-                // same key/IV. It's actually fine here, because the data is the same, but any
-                // sane implementation will throw an exception.
-                // Request a new cipher using the same materials to avoid reinit issues
-                // TODO: This can probably be moved into CipherAsyncRequestBody
-                cipher = CipherProvider.createAndInitCipher(materials, iv);
-            }
+            outputBuffer = cipher.update(buf, 0, amountToReadFromByteBuffer);
             if (outputBuffer == null && amountToReadFromByteBuffer < cipher.getBlockSize()) {
                 // The underlying data is too short to fill in the block cipher
                 // This is true at the end of the file, so complete to get the final
