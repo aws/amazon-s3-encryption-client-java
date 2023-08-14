96dd9f7a864f867854b694b82d0fe623f2a168a6
Darwin Chowdary <imabhichow@gmail.com>
2023-08-14 01:00:43 -0700

    modify CI to run tests parallel and run daily

--- src/main/java/software/amazon/encryption/s3/materials/GenerateDataKeyStrategy.java
+++ src/main/java/software/amazon/encryption/s3/materials/GenerateDataKeyStrategy.java
@@ -1,8 +1,5 @@
 package software.amazon.encryption.s3.materials;
 
-import java.security.GeneralSecurityException;
-import java.security.SecureRandom;
-
 public interface GenerateDataKeyStrategy {
     String keyProviderInfo();
 
