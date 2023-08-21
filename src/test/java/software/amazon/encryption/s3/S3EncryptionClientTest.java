926818b0febbf823839a5053b6d9d5a25352faab
Darwin Chowdary <39110935+imabhichow@users.noreply.github.com>
2023-08-21 09:56:37 -0700

    chore: fix bugs and nit (#175)
    * ci-workflow.yml - set fail-fast to false
    * cfn/S3EC-GitHub-CF-Template - Remove Encrypt for IAM role
    * S3EncryptionClient.java - Initialize Multipart Configuration when no override configuration is provided.
    * AlgorithmSuite - Clean-up code

--- src/test/java/software/amazon/encryption/s3/S3EncryptionClientTest.java
+++ src/test/java/software/amazon/encryption/s3/S3EncryptionClientTest.java
@@ -46,7 +46,6 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
