5e045c3d8ed2f638c19f2d20f9233621cd02397b
Kess Plasmeier <kessplas@amazon.com>
2024-10-27 16:26:26 -0700

    log exception

--- src/test/java/software/amazon/encryption/s3/S3EncryptionClientTest.java
+++ src/test/java/software/amazon/encryption/s3/S3EncryptionClientTest.java
@@ -624,6 +624,7 @@ public class S3EncryptionClientTest {
                     .key(objectKey));
             fail("expected exception");
         } catch (S3EncryptionClientException ex) {
+            System.out.println(ex.getMessage());
             assertTrue(ex.getMessage().contains("Instruction file not found!"));
             assertEquals(ex.getCause().getClass(), S3EncryptionClientException.class);
         }
