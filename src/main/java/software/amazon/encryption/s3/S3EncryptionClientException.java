c0ab3cf9002a8ed1c43262bed5f2ede14f1f17c2
rishav-karanjit <karanjitrishav4@gmail.com>
2025-03-04 15:03:25 -0800

    auto commit

--- src/main/java/software/amazon/encryption/s3/S3EncryptionClientException.java
+++ src/main/java/software/amazon/encryption/s3/S3EncryptionClientException.java
@@ -6,6 +6,10 @@ import software.amazon.awssdk.core.exception.SdkClientException;
 
 public class S3EncryptionClientException extends SdkClientException {
 
+    private S3EncryptionClientException(Builder b) {
+        super(b);
+    }
+
     public S3EncryptionClientException(String message) {
         super(SdkClientException.builder()
                 .message(message));
@@ -15,5 +19,52 @@ public class S3EncryptionClientException extends SdkClientException {
         super(SdkClientException.builder()
                 .message(message)
                 .cause(cause));
+    }
+
+    @Override
+    public Builder toBuilder() {
+        return new BuilderImpl(this);
+    }
+
+    public static Builder builder() {
+        return new BuilderImpl();
+    }
+
+    public interface Builder extends SdkClientException.Builder {
+        @Override
+        Builder message(String message);
+
+        @Override
+        Builder cause(Throwable cause);
+
+        @Override
+        S3EncryptionClientException build();
+    }
+
+    protected static final class BuilderImpl extends SdkClientException.BuilderImpl implements Builder {
+
+        protected BuilderImpl() {
+        }
+
+        protected BuilderImpl(S3EncryptionClientException ex) {
+            super(ex);
+        }
+
+        @Override
+        public Builder message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        @Override
+        public Builder cause(Throwable cause) {
+            this.cause = cause;
+            return this;
+        }
+
+        @Override
+        public S3EncryptionClientException build() {
+            return new S3EncryptionClientException(this);
+        }
     }
 }
