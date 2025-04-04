5016315bebf964d9aec6400e82f55d4025066232
Kess Plasmeier <kessplas@amazon.com>
2025-04-04 15:19:47 -0700

    improve other example tests

--- src/test/java/software/amazon/encryption/s3/examples/ClientConfigurationExampleTest.java
+++ src/test/java/software/amazon/encryption/s3/examples/ClientConfigurationExampleTest.java
@@ -2,9 +2,16 @@ package software.amazon.encryption.s3.examples;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class ClientConfigurationExampleTest {
   @Test
   public void testClientConfigurationExamples() {
-      ClientConfigurationExample.main(new String[0]);
+      try {
+          ClientConfigurationExample.main(new String[0]);
+      } catch (Throwable exception) {
+          exception.printStackTrace();
+          fail("Client Configuration Example Test Failed!!", exception);
+      }
   }
 }
--- src/test/java/software/amazon/encryption/s3/examples/MultipartUploadExampleTest.java
+++ src/test/java/software/amazon/encryption/s3/examples/MultipartUploadExampleTest.java
@@ -3,14 +3,12 @@ package software.amazon.encryption.s3.examples;
 import org.junit.jupiter.api.Test;
 import software.amazon.encryption.s3.utils.S3EncryptionClientTestResources;
 
-import java.io.IOException;
-
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class MultipartUploadExampleTest {
 
     @Test
-    public void testMultipartUploadExamples() throws IOException {
+    public void testMultipartUploadExamples() {
         final String bucket = S3EncryptionClientTestResources.BUCKET;
         try {
             MultipartUploadExample.main(new String[]{bucket});
