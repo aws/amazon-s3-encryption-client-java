3bd85560975c7aa316a740b32b3093aa7c8bf5a2
Anirav Kareddy <aniravk@amazon.com>
2025-06-18 14:53:03 -0700

    removed unused import

--- src/main/java/software/amazon/encryption/s3/materials/MaterialsDescription.java
+++ src/main/java/software/amazon/encryption/s3/materials/MaterialsDescription.java
@@ -6,7 +6,6 @@ package software.amazon.encryption.s3.materials;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
