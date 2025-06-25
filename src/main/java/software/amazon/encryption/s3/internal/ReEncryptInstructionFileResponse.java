f501b43fe888639728adb0fa6797cae8d5b74786
Anirav Kareddy <aniravk@amazon.com>
2025-06-25 15:25:32 -0700

    cleaned up ReEncryptInstructionFileResponse class

--- src/main/java/software/amazon/encryption/s3/internal/ReEncryptInstructionFileResponse.java
+++ src/main/java/software/amazon/encryption/s3/internal/ReEncryptInstructionFileResponse.java
@@ -3,7 +3,6 @@ package software.amazon.encryption.s3.internal;
 /** Response object for re-encrypting instruction files.
  * Contains the bucket, key, and instruction file suffix of the re-encrypted instruction file in S3.
  */
-
 public class ReEncryptInstructionFileResponse {
   private final String bucket;
   private final String key;
@@ -14,12 +13,15 @@ public class ReEncryptInstructionFileResponse {
     this.key = key;
     this.instructionFileSuffix = instructionFileSuffix;
   }
+
   public String Bucket() {
     return bucket;
   }
+
   public String Key() {
     return key;
   }
+
   public String InstructionFileSuffix() {
     return instructionFileSuffix;
   }
