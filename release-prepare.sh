9c0143c69c8bdf41321b673ea5b38355220a546f
Darwin Chowdary <39110935+imabhichow@users.noreply.github.com>
2025-12-16 17:41:54 -0800

    chore: update releaserc

--- release-prepare.sh
+++ release-prepare.sh
@@ -9,15 +9,26 @@ MAJOR_VERSION=$(echo "$VERSION" | cut -d. -f1)
 
 echo "Preparing release for version $VERSION (major: $MAJOR_VERSION)"
 
+# Update Maven versions
 mvn versions:set -DnewVersion="$VERSION" -DautoVersionSubmodules=true
 
 # Portable sed for macOS and Linux
-SED="sed -i"
-[[ "$OSTYPE" == "darwin"* ]] && SED="sed -i ''"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  SED_CMD="sed -i ''"
+else
+  SED_CMD="sed -i"
+fi
 
-$SED "s/<s3ec.version>.*<\/s3ec.version>/<s3ec.version>$VERSION<\/s3ec.version>/g" migration_examples/v3-to-v4/v4/pom.xml
-$SED "s/API_VERSION_UNKNOWN = \".*-unknown\"/API_VERSION_UNKNOWN = \"$MAJOR_VERSION-unknown\"/g" src/main/java/software/amazon/encryption/s3/internal/ApiNameVersion.java
-$SED "s/EXPECTED_API_MAJOR_VERSION = \"[0-9]*\"/EXPECTED_API_MAJOR_VERSION = \"$MAJOR_VERSION\"/g" src/test/java/software/amazon/encryption/s3/internal/ApiNameVersionTest.java
-$SED "s/<version>.*<\/version>/<version>$VERSION<\/version>/g" README.md
+# Update s3ec version in migration examples
+$SED_CMD "s/<s3ec.version>.*<\/s3ec.version>/<s3ec.version>$VERSION<\/s3ec.version>/g" migration_examples/v3-to-v4/v4/pom.xml
+
+# Update API_VERSION_UNKNOWN with major version
+$SED_CMD "s/public static final String API_VERSION_UNKNOWN = \".*-unknown\"/public static final String API_VERSION_UNKNOWN = \"$MAJOR_VERSION-unknown\"/g" src/main/java/software/amazon/encryption/s3/internal/ApiNameVersion.java
+
+# Update EXPECTED_API_MAJOR_VERSION
+$SED_CMD "s/EXPECTED_API_MAJOR_VERSION = \"[0-9]*\"/EXPECTED_API_MAJOR_VERSION = \"$MAJOR_VERSION\"/g" src/test/java/software/amazon/encryption/s3/internal/ApiNameVersionTest.java
+
+# Update version in README
+$SED_CMD "s/<version>.*<\/version>/<version>$VERSION<\/version>/g" README.md
 
 echo "Release preparation complete"
