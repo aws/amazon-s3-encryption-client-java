d423d8d0f500c6fcc7adb703a1626d6e7d4c2e3d
Darwin Chowdary <39110935+imabhichow@users.noreply.github.com>
2025-12-16 18:43:02 -0800

    chore: update releaserc (#492)
    * chore: update releaserc
    
    * chore: update releaserc
    
    * remove verify

--- release-prepare.sh
+++ release-prepare.sh
@@ -12,16 +12,23 @@ echo "Preparing release for version $VERSION (major: $MAJOR_VERSION)"
 # Update Maven versions
 mvn versions:set -DnewVersion="$VERSION" -DautoVersionSubmodules=true
 
+# Portable sed for macOS and Linux
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  SED_CMD="sed -i ''"
+else
+  SED_CMD="sed -i"
+fi
+
 # Update s3ec version in migration examples
-sed -i '' "s/<s3ec.version>.*<\/s3ec.version>/<s3ec.version>$VERSION<\/s3ec.version>/g" migration_examples/v3-to-v4/v4/pom.xml
+$SED_CMD "s/<s3ec.version>.*<\/s3ec.version>/<s3ec.version>$VERSION<\/s3ec.version>/g" migration_examples/v3-to-v4/v4/pom.xml
 
 # Update API_VERSION_UNKNOWN with major version
-sed -i '' "s/public static final String API_VERSION_UNKNOWN = \".*-unknown\"/public static final String API_VERSION_UNKNOWN = \"$MAJOR_VERSION-unknown\"/g" src/main/java/software/amazon/encryption/s3/internal/ApiNameVersion.java
+$SED_CMD "s/public static final String API_VERSION_UNKNOWN = \".*-unknown\"/public static final String API_VERSION_UNKNOWN = \"$MAJOR_VERSION-unknown\"/g" src/main/java/software/amazon/encryption/s3/internal/ApiNameVersion.java
 
 # Update EXPECTED_API_MAJOR_VERSION
-sed -i '' "s/EXPECTED_API_MAJOR_VERSION = \"[0-9]*\"/EXPECTED_API_MAJOR_VERSION = \"$MAJOR_VERSION\"/g" src/test/java/software/amazon/encryption/s3/internal/ApiNameVersionTest.java
+$SED_CMD "s/EXPECTED_API_MAJOR_VERSION = \"[0-9]*\"/EXPECTED_API_MAJOR_VERSION = \"$MAJOR_VERSION\"/g" src/test/java/software/amazon/encryption/s3/internal/ApiNameVersionTest.java
 
 # Update version in README
-sed -i '' "s/<version>.*<\/version>/<version>$VERSION<\/version>/g" README.md
+$SED_CMD "s/<version>.*<\/version>/<version>$VERSION<\/version>/g" README.md
 
 echo "Release preparation complete"
