2f194c13c070b1fc225210f8aea990b6b4facf6b
texastony <5892063+texastony@users.noreply.github.com>
2026-02-20 10:46:32 -0600

    test(examples): Instruction Files
    - Add KMS Keyring Instruction File example
    - Test KMS Keyring Instruction File example
    - Test ReEncrypt Instruction File examples

---
+++ src/test/java/software/amazon/encryption/s3/examples/ReEncryptInstructionFileExampleTest.java
@@ -0,0 +1,42 @@
+package software.amazon.encryption.s3.examples;
+
+import org.junit.jupiter.api.Test;
+import software.amazon.encryption.s3.utils.S3EncryptionClientTestResources;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ReEncryptInstructionFileExampleTest {
+
+    @Test
+    public void testSimpleAesKeyringReEncryptInstructionFile() {
+        final String bucket = S3EncryptionClientTestResources.BUCKET;
+        try {
+            ReEncryptInstructionFileExample.simpleAesKeyringReEncryptInstructionFile(bucket);
+        } catch (Throwable exception) {
+            exception.printStackTrace();
+            fail("AES Keyring ReEncrypt Instruction File Test Failed!!", exception);
+        }
+    }
+
+    @Test
+    public void testSimpleRsaKeyringReEncryptInstructionFile() {
+        final String bucket = S3EncryptionClientTestResources.BUCKET;
+        try {
+            ReEncryptInstructionFileExample.simpleRsaKeyringReEncryptInstructionFile(bucket);
+        } catch (Throwable exception) {
+            exception.printStackTrace();
+            fail("RSA Keyring ReEncrypt Instruction File Test Failed!!", exception);
+        }
+    }
+
+    @Test
+    public void testSimpleRsaKeyringReEncryptInstructionFileWithCustomSuffix() {
+        final String bucket = S3EncryptionClientTestResources.BUCKET;
+        try {
+            ReEncryptInstructionFileExample.simpleRsaKeyringReEncryptInstructionFileWithCustomSuffix(bucket);
+        } catch (Throwable exception) {
+            exception.printStackTrace();
+            fail("RSA Keyring ReEncrypt Instruction File With Custom Suffix Test Failed!!", exception);
+        }
+    }
+}
