de00ef1fa7804447dfad93f10cb69a3ae7c893c2
Tony Knapp <5892063+texastony@users.noreply.github.com>
2026-02-24 14:21:24 -0600

    test(examples): Instruction Files (#505)

---
+++ src/test/java/software/amazon/encryption/s3/examples/InstructionFileExampleTest.java
@@ -0,0 +1,21 @@
+package software.amazon.encryption.s3.examples;
+
+import org.junit.jupiter.api.Test;
+import software.amazon.encryption.s3.utils.S3EncryptionClientTestResources;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class InstructionFileExampleTest {
+
+    @Test
+    public void testInstructionFileExample() {
+        final String bucket = S3EncryptionClientTestResources.BUCKET;
+        final String kmsKeyId = S3EncryptionClientTestResources.KMS_KEY_ID;
+        try {
+            InstructionFileExample.simpleKmsKeyringUseInstructionFile(bucket, kmsKeyId);
+        } catch (Throwable exception) {
+            exception.printStackTrace();
+            fail("Instruction File Example Test Failed!!", exception);
+        }
+    }
+}
